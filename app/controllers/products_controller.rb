56b0b80d0c5f8b601891979ad7e7893882e61f53
Lando <olandomcdonald07@gmail.com>
2025-11-28 23:24:05 -0600

    Fixed css after adding bulma cdn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,11 +2,17 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
 
+    # Filter by category
     if params[:category].present?
       @products = @products.where(category: params[:category])
     end
 
-    @products = @products.order(:name)
+    # Filter by search
+    if params[:search].present?
+      @products = @products.where("name LIKE ? OR decription LIKE ?", "%#{params[:search]}%", "%#{params[:search]}%")
+    end
+
+    @products = @products.page(params[:page]).per(12)
   end
 
   def show
