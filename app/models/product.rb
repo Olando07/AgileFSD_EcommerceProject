ccc7581766c795e8fdc1ad484f4c1d6a8f4ddce1
Lando <olandomcdonald07@gmail.com>
2025-11-30 18:01:08 -0600

    Add products cards and edit price validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,7 +5,7 @@ class Product < ApplicationRecord
 
   validates :name, presence: true
   validates :description, presence: true
-  validates :price, presence: true, numericality: { greater_than: 0 }
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :category, presence: true
 
   def self.ransackable_associations(auth_object = nil)
