995bd7a95d4d390c62fb64ed3f2000e413d7fdd5
Lando <olandomcdonald07@gmail.com>
2025-12-03 11:32:40 -0600

    Add address fields to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_12_03_030351) do
+ActiveRecord::Schema[8.1].define(version: 2025_12_03_173030) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -113,11 +113,13 @@ ActiveRecord::Schema[8.1].define(version: 2025_12_03_030351) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "city"
     t.datetime "created_at", null: false
     t.string "email"
     t.boolean "is_admin"
     t.string "name"
     t.string "password_digest"
+    t.string "postal_code"
     t.integer "province_id", null: false
     t.string "street_address"
     t.string "telephone"
