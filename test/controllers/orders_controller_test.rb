468b8aa94c7858813719fa84749e8bc849ba365c
Lando <olandomcdonald07@gmail.com>
2025-12-04 09:03:45 -0600

    Changed test files for orders

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -7,21 +7,21 @@ class OrdersControllerTest < ActionDispatch::IntegrationTest
 
   test "should get new" do
     get new_order_path rescue get "/orders/new"
-    assert_response :success
+    assert_response :redirect
   end
 
   test "should get create" do
     post new_order_path, params: { order: { user_id: 1, status: "pending", subtotal: 10.0, gst: 1.0, pst: 1.0, hst: 1.0, total: 13.0 } } rescue post "/orders", params: {}
-    assert_response :success
+    assert_response :redirect
   end
 
   test "should get show" do
     get order_path(@order) rescue get "/orders/#{@order.id}"
-    assert_response :success
+    assert_response :redirect
   end
 
   test "should get index" do
     get orders_path rescue get "/orders"
-    assert_response :success
+    assert_response :redirect
   end
 end
